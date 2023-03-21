f4ea656373cfd21d18ab01a6166e7ed03a7eadd1
Alexm-99 <alexmedranda_99@hotmail.com>
2023-03-20 22:55:51 -0500

    add module platform

---
+++ scenas/module/platform.js
@@ -0,0 +1,18 @@
+export class Platform {
+    // constructor(x, y, imagen, group) {
+    //     super(x, y, imagen, group);
+
+    
+
+
+    crearPlataforma(x, y, imagen, group) {
+        const scale = 1.8;
+        const plataforma = group.create(x, y, imagen).setScale(scale);
+      
+        /*   plataforma.body.setSize(plataforma.width * scale, plataforma.height * scale);
+        plataforma.body.setOffset(x, y); */
+        return plataforma;
+      }
+      
+
+}
