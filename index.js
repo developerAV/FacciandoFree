d095ea84c1a65c6c41eb284d776dc81d8b714a55
Alexm-99 <alexmedranda_99@hotmail.com>
2023-08-14 01:35:13 -0500

    nueva escena cubicle

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { ComputerRoom } from "./scenes/computer_room.js";
 import { Laboratorio1 } from "./scenes/electronic_room.js";
 import { Hallway2 } from "./scenes/hallway2.js";
 import { Hallway300 } from "./scenes/hallway300.js";
+import { Cubicle } from "./scenes/cubicle.js";
 /* import {Player} from './scenas/player.js';
  */
 
@@ -14,7 +15,7 @@ const config = {
   height: 1000,
   backgroundColor: "#000",
   // transparent: true, // Comentado porque no se usa
-  scene: [Intro, Aula, ComputerRoom, Laboratorio1, Hallway2, Hallway300],
+  scene: [Intro, Aula, ComputerRoom, Laboratorio1, Hallway2, Hallway300, Cubicle],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
--- scenes/intro.js
+++ scenes/intro.js
@@ -136,7 +136,7 @@ function blurButton(boton, escena) {
         500,
         () => {
           // Esperar 500 milisegundos antes de cambiar de escena
-          escena.scene.start("hallway300");
+          escena.scene.start("cubicle");
           // escena.scene.start("lab1");
         },
         [],
