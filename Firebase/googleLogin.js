f4992cda31c8d10672d01dffec54f5a31be41248
Alexm-99 <alexmedranda_99@hotmail.com>
2023-09-01 21:32:26 -0500

    button language| login update

--- Firebase/googleLogin.js
+++ Firebase/googleLogin.js
@@ -1,22 +1,21 @@
 import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
 import { auth } from "./firebase.js";
-// import { showMessage } from "./showMessage.js";
 
-// const googleButton = document.querySelector("#googleLogin");
 export const loginGoogle = async(escena) => {
 
   const provider = new GoogleAuthProvider();
+//"google sign in" mostrar en el prompt de google el usuario y no el dominio de la cuenta que se esta usando
+  provider.setCustomParameters({
+    prompt: "select_account",
+  });
+
+
   try {
     const credentials = await signInWithPopup(auth, provider)
     // console.log(credentials);
     console.log("google sign in");
     escena.scene.start("intro");
-    // Close the login modal
-    // const modalInstance = bootstrap.Modal.getInstance(googleButton.closest('.modal'));
-    // modalInstance.hide();
-
-    // show welcome message
-    // showMessage("Welcome " + credentials.user.displayName);
+    
   } catch (error) {
     console.log(error);
   }
